9b0edfb8793a3e579afd221e60727ffd98b49c15
Jackeline Lima <jackelinelimap@gmail.com>
2022-10-26 21:12:02 -0300

    init api

--- QueryBooks/QueryBooks/Networking.swift
+++ QueryBooks/QueryBooks/Networking.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+
+enum Errors: Error {
+    case noData
+    case decodeFailed
+    case noURL
+}
+
+class Network {
+
+    let baseUrl = "https://sandbox.mercadoeditorial.org/api/v1.2/book?codigo_status=1"
+    let baseUrlFilter = "https://sandbox.mercadoeditorial.org/api/v1.2/book"
+
+    func getBooks(completion: @escaping (Result<Product, Errors>) -> Void) {
+
+        guard let url = URL(string: baseUrl) else {
+            completion(.failure(.noURL))
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            do{
+                let jsonResult = try JSONDecoder().decode(Product.self, from: data)
+                completion(Result.success(jsonResult))
+            } catch {
+                completion(Result.failure(.decodeFailed))
+            }
+        }
+
+        task.resume()
+    }
+
+    func filterBooksForTitle(with title: String, completion: @escaping (Result<Product, Errors>) -> Void) {
+
+        guard let url = URL(string: "\(baseUrlFilter)?titulo=\(title)") else {
+            completion(.failure(.noURL))
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            do{
+                let jsonResult = try JSONDecoder().decode(Product.self, from: data)
+                completion(Result.success(jsonResult))
+            } catch {
+                completion(Result.failure(.decodeFailed))
+            }
+        }
+        task.resume()
+    }
+}
